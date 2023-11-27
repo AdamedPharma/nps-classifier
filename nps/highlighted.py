93b0f5f899705624eeef85831b1a6fa9918edcc4
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2023-11-27 09:47:10 +0100

    Update highlighted.py

--- nps/highlighted.py
+++ nps/highlighted.py
@@ -1,40 +1,35 @@
-
-from nps12 import classifier
+from nps import nps12
 from nps.nps12 import systems_map_I, systems_map_II
-# from nps12 import systems_map_I, systems_map_II
-import io
-
-smiles = "O1C(CCN)=CC2=CC3=C(C=CO3)C=C12"
+from rdkit.Chem import Draw
+from rdkit import Chem
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO
 
 
 def highlighted_mol(smiles: str):
 
-    from nps12 import classifier
-    # from nps.nps12 import systems_map_I, systems_map_II
-    from nps12 import systems_map_I, systems_map_II
-    import io
-    _, _, suspected = classifier(smiles, systems_map_I)
+    _, _, suspected_I = nps12.classifier(smiles, systems_map_I)
+    _, _, suspected_II = nps12.classifier(smiles, systems_map_II)
 
-    from rdkit.Chem import Draw
-    from rdkit import Chem
-    import matplotlib.pyplot as plt
-    import numpy as np
-
+    suspected = ()
     mol = Chem.MolFromSmiles(smiles)
+
+    if suspected_II:
+        suspected = suspected_II
+    if suspected_I:
+        suspected = suspected_I
+
     img = Draw.MolsToGridImage([mol], molsPerRow=1,
                                highlightAtomLists=[list(suspected)], subImgSize=(1200, 1200))
     main_mol = np.array(img)
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.axis("off")
     plt.imshow(main_mol)
-    buf = io.BytesIO()
-    plt.savefig(buf, format="png")
-    buf.seek(0)
+    imagefile = BytesIO()
+    img.save(imagefile, format="PNG")
+    imagedata = imagefile.getvalue()
 
-    with open("output.png", "wb") as f:
-        f.write(buf.read())
+    return imagedata
 
-    buf.close()
-
-
-highlighted_mol(smiles)
+# highlighted_mol("O=C(CN[C@@H](Cc1ccc(OC)cc1)C)c2ccc(O)c(c2)N")
