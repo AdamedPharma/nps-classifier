417f1b0f04bd3bf2744c181281c05525ef39a403
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2023-12-01 15:29:34 +0100

    Update clf.py

--- nps/clf.py
+++ nps/clf.py
@@ -4,8 +4,8 @@ from nps.nps12 import systems_map_I, systems_map_II
 
 def main(smiles: str):
 
-    res_I, _, _ = nps12.classifier(smiles, systems_map_I)
-    res_II, _, _ = nps12.classifier(smiles, systems_map_II)
+    res_I, _, _, _ = nps12.classifier(smiles, systems_map_I)
+    res_II, _, _, _ = nps12.classifier(smiles, systems_map_II)
 
     if res_II:
         predict = "NPS_II"
