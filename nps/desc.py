e6cfd914054be40fcb4727f5d1248aabe2578c10
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2023-12-01 15:37:33 +0100

    Update desc.py

--- nps/desc.py
+++ nps/desc.py
@@ -4,8 +4,8 @@ from nps.nps12 import systems_map_I, systems_map_II
 
 def main(smiles: str):
 
-    res_I, desc_I, _ = nps12.classifier(smiles, systems_map_I)
-    res_II, desc_II, _ = nps12.classifier(smiles, systems_map_II)
+    res_I, desc_I, _, _ = nps12.classifier(smiles, systems_map_I)
+    res_II, desc_II, _, _ = nps12.classifier(smiles, systems_map_II)
 
     if res_II:
         description = desc_II
