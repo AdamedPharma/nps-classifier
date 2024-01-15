cff1db992d16d750034ca648e1c42ea555e04553
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2024-01-15 13:50:04 +0100

    Update desc.py

--- nps/desc.py
+++ nps/desc.py
@@ -13,27 +13,49 @@ from nps.nps6 import systems_map_VI
 
 def main(smiles: str):
 
-    res_I, desc_I, _, _ = nps12.classifier(smiles, systems_map_I)
-    res_II, desc_II, _, _ = nps12.classifier(smiles, systems_map_II)
-    res_III, desc_III, _, _ = nps3.classifier(smiles, systems_map_III)
-    res_IV, desc_IV, _, _ = nps4.classifier(smiles, systems_map_IV)
-    res_V, desc_V, _, _ = nps5.classifier(smiles, systems_map_V)
-    res_VI, desc_VI, _, _ = nps6.classifier(smiles, systems_map_VI)
-
-    if res_VI:
-        description = desc_VI
-    elif res_V:
-        description = desc_V
-    elif res_IV:
-        description = desc_IV
-    elif res_II:
-        description = desc_II
-    elif res_I:
-        description = desc_I
-    elif res_III:
-        description = desc_III
-    else:
-        description = ""
+    if nps12.classifier(smiles, systems_map_I):
+        res_I, desc_I, _, _ = nps12.classifier(smiles, systems_map_I)
+    
+    elif nps12.classifier(smiles, systems_map_II):
+        res_II, desc_II, _, _ = nps12.classifier(smiles, systems_map_II)
+    
+    elif nps3.classifier(smiles, systems_map_III):
+        res_III, desc_III, _, _ = nps3.classifier(smiles, systems_map_III)
         
-    return description
+    elif nps4.classifier(smiles, systems_map_IV):
+        res_IV, desc_IV, _, _ = nps4.classifier(smiles, systems_map_IV)
+        
+    elif nps5.classifier(smiles, systems_map_V):
+        res_V, desc_V, _, _ = nps5.classifier(smiles, systems_map_V)
+    elif nps6.classifier(smiles, systems_map_VI):
+        res_VI, desc_VI, _, _ = nps6.classifier(smiles, systems_map_VI)
+        
+    else:
+        description = "Do weryfikacji."
+
+return description
+    
+    # res_I, desc_I, _, _ = nps12.classifier(smiles, systems_map_I)
+    # res_II, desc_II, _, _ = nps12.classifier(smiles, systems_map_II)
+    # res_III, desc_III, _, _ = nps3.classifier(smiles, systems_map_III)
+    # res_IV, desc_IV, _, _ = nps4.classifier(smiles, systems_map_IV)
+    # res_V, desc_V, _, _ = nps5.classifier(smiles, systems_map_V)
+    # res_VI, desc_VI, _, _ = nps6.classifier(smiles, systems_map_VI)
+
+    # if res_VI:
+    #     description = desc_VI
+    # elif res_V:
+    #     description = desc_V
+    # elif res_IV:
+    #     description = desc_IV
+    # elif res_II:
+    #     description = desc_II
+    # elif res_I:
+    #     description = desc_I
+    # elif res_III:
+    #     description = desc_III
+    # else:
+    #     description = ""
+        
+    # return description
     
