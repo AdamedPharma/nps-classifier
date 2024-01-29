b4e2aea5038b8ec49606d0b624d5cbde4dfa9b17
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2024-01-29 10:04:13 +0100

    Update clf.py

--- nps/clf.py
+++ nps/clf.py
@@ -21,27 +21,27 @@ def main(smiles: str):
 
     if nps5.classifier(smiles, systems_map_V):
         res_V, _, _, _ = nps5.classifier(smiles, systems_map_V)
-        if res_V:
+        if res_V and predict == "nie podlega":
             predict = "NPS_V"
 
     if nps4.classifier(smiles, systems_map_IV):
         res_IV, _, _, _ = nps4.classifier(smiles, systems_map_IV)
-        if res_IV:
+        if res_IV and predict == "nie podlega":
             predict = "NPS_IV"
 
     if nps12.classifier(smiles, systems_map_II):
         res_II, _, _, _ = nps12.classifier(smiles, systems_map_II)
-        if res_II:
+        if res_II and predict == "nie podlega":
             predict = "NPS_II"
 
     if nps12.classifier(smiles, systems_map_I):
         res_I, _, _, _ = nps12.classifier(smiles, systems_map_I)
-        if res_I:
+        if res_I and predict == "nie podlega":
             predict = "NPS_I"
         
     if nps3.classifier(smiles, systems_map_III):
         res_III, _, _, _ = nps3.classifier(smiles, systems_map_III)
-        if res_III:
+        if res_III and predict == "nie podlega":
             predict = "NPS_III" 
 
     return predict
