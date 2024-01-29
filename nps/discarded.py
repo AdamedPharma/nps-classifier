a671c948f2f5e3d7c301e8dc5f8920b9f2ee4c9b
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2024-01-29 09:46:35 +0100

    Update discarded.py

--- nps/discarded.py
+++ nps/discarded.py
@@ -4,7 +4,7 @@ from nps.nps12 import systems_map_I
 def main(smiles: str):
 
     description = ""
-    if nps12.classifier(smiles, systems_map_I)::
+    if nps12.classifier(smiles, systems_map_I):
         res_I, desc_I, _, _ = nps12.classifier(smiles, systems_map_I)
     else:
         description = "Do weryfikacji"
