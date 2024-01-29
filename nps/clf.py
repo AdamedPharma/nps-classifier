c255b9176f18894a5dde995134b5933254263c5c
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2024-01-29 07:48:03 +0100

    Update clf.py

--- nps/clf.py
+++ nps/clf.py
@@ -15,22 +15,22 @@ def main(smiles: str):
     
     res_I, _, _, _ = nps12.classifier(smiles, systems_map_I)
     res_II, _, _, _ = nps12.classifier(smiles, systems_map_II)
-    # res_III, _, _, _ = nps3.classifier(smiles, systems_map_III)
-    # res_IV, _, _, _ = nps4.classifier(smiles, systems_map_IV)
-    # res_V, _, _, _ = nps5.classifier(smiles, systems_map_V)
-    # res_VI, _, _, _ = nps6.classifier(smiles, systems_map_VI)
+    res_III, _, _, _ = nps3.classifier(smiles, systems_map_III)
+    res_IV, _, _, _ = nps4.classifier(smiles, systems_map_IV)
+    res_V, _, _, _ = nps5.classifier(smiles, systems_map_V)
+    res_VI, _, _, _ = nps6.classifier(smiles, systems_map_VI)
 
-    # if res_VI:
-    #     predict = "NPS_VI"
-    # elif res_V:
-    #     predict = "NPS_V"
-    # elif res_IV:
-    #     predict = "NPS_IV"
-    # elif res_II:
-    #     predict = "NPS_II"
+    if res_VI:
+        predict = "NPS_VI"
+    elif res_V:
+        predict = "NPS_V"
+    elif res_IV:
+        predict = "NPS_IV"
+    elif res_II:
+        predict = "NPS_II"
     if res_I:
         predict = "NPS_I"
-    elif res_II:
+    elif res_III:
         predict = "NPS_III"
     else:
         predict = "nie podlega"
