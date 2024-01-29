f57bd38b1aaa080c00f3a7e3601cd798eec72064
Paulina Duda <80198325+pd410668@users.noreply.github.com>
2024-01-29 09:03:10 +0100

    Update clf.py

--- nps/clf.py
+++ nps/clf.py
@@ -19,27 +19,27 @@ def main(smiles: str):
         if res_VI:
             predict = "NPS_VI"
 
-    elif nps5.classifier(smiles, systems_map_V):
+    if nps5.classifier(smiles, systems_map_V):
         res_V, _, _, _ = nps5.classifier(smiles, systems_map_V)
         if res_V:
             predict = "NPS_V"
 
-    elif nps4.classifier(smiles, systems_map_IV):
+    if nps4.classifier(smiles, systems_map_IV):
         res_IV, _, _, _ = nps4.classifier(smiles, systems_map_IV)
         if res_IV:
             predict = "NPS_IV"
 
-    elif nps12.classifier(smiles, systems_map_II):
+    if nps12.classifier(smiles, systems_map_II):
         res_II, _, _, _ = nps12.classifier(smiles, systems_map_II)
         if res_II:
             predict = "NPS_II"
 
-    elif nps12.classifier(smiles, systems_map_I):
+    if nps12.classifier(smiles, systems_map_I):
         res_I, _, _, _ = nps12.classifier(smiles, systems_map_I)
         if res_I:
             predict = "NPS_I"
         
-    elif nps3.classifier(smiles, systems_map_III):
+    if nps3.classifier(smiles, systems_map_III):
         res_III, _, _, _ = nps3.classifier(smiles, systems_map_III)
         if res_III:
             predict = "NPS_III"
